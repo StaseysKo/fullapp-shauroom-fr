5a51e6df65f50b6056a1c7b57d161c6eab6ac10d
Staseys <kolyubatskystas@gmail.com>
2022-10-20 00:55:08 +0600

    download app

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+// ----------------------------------------------------------------------
+
+const nextConfig = {
+  trailingSlash: true,
+  env: {
+    DEV_API: '',
+    PRODUCTION_API: '',
+    GOOGLE_API: '',
+  },
+  images: {
+    domains: ['flagcdn.com'],
+  },
+};
+
+export default nextConfig;
